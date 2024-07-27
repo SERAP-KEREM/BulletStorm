376e926ad7af765bd0c93982af53f6d69521c305
Serap Kerem <66235306+SERAP-KEREM@users.noreply.github.com>
2024-07-27 23:18:11 +0300

    vector add.

--- Assets/Scripts/Player/FaceObjectToCamera.cs
+++ Assets/Scripts/Player/FaceObjectToCamera.cs
@@ -1,12 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 using UnityEngine;
 
 public class FaceObjectToCamera : MonoBehaviour
 {
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        // Coroutine başlat
+        StartCoroutine(FindMainCamera());
+    }
 
     private void Update()
     {
-        transform.LookAt(Camera.main.transform);   
+        if (mainCamera != null)
+        {
+            transform.LookAt(mainCamera.transform);
+        }
+    }
+
+    private IEnumerator FindMainCamera()
+    {
+        // Camera.main bulunana kadar bekle
+        while (Camera.main == null)
+        {
+            yield return null; // Bir frame bekle
+        }
+
+        // Camera.main atandıktan sonra mainCamera değişkenine ata
+        mainCamera = Camera.main;
     }
 }
--- Assets/Scripts/Player/Weapon.cs
+++ Assets/Scripts/Player/Weapon.cs
@@ -7,10 +7,8 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     public int damage;
-
     public float fireRate;
     private float nextFire;
-
     public Camera camera;
 
     [Header("VFX")]
@@ -18,12 +16,10 @@ public class Weapon : MonoBehaviour
 
     [Header("Ammo")]
     public int fireCount = 30;
-
     public int ammo = 30;
     public int magAmmo = 150;
 
     [Header("UI")]
-   // public TextMeshProUGUI magText;
     public TextMeshProUGUI ammoText;
 
     [Header("Animation")]
@@ -31,35 +27,24 @@ public class Weapon : MonoBehaviour
     public AnimationClip reload;
 
     [Header("Recoil Settings")]
-    //[Range(0, 1)]
-    //public float recoilPercent = 0.3f;
-
     [Range(0, 2)]
     public float recoverPercent = 0.7f;
 
-    [Space]
     public float recoilUp = 1f;
     public float recoilBack = 0f;
 
     private Vector3 originalPosition;
-    private Vector3 recoilVelocity=Vector3.zero;
-
+    private Vector3 recoilVelocity = Vector3.zero;
     private float recoilLength;
     private float recoverLenth;
-
     private bool recoiling;
     public bool recovering;
 
-
-
     private void Start()
     {
-      //  magText.text = mag.ToString();
         ammoText.text = ammo + "/" + magAmmo.ToString();
-
-        originalPosition=transform.localPosition;
-
-        recoilLength =0;
+        originalPosition = transform.localPosition;
+        recoilLength = 0;
         recoverLenth = 1 / fireRate * recoverPercent;
     }
 
@@ -70,63 +55,52 @@ public class Weapon : MonoBehaviour
             nextFire -= Time.deltaTime;
         }
 
-        if (Input.GetButton("Fire1") && nextFire <= 0 && ammo>0 && weaponAnim.isPlaying==false)
+        if (Input.GetButton("Fire1") && nextFire <= 0 && ammo > 0 && weaponAnim.isPlaying == false)
         {
             Debug.Log("ateş");
             nextFire = 1 / fireRate;
             ammo--;
-
-           // magText.text = mag.ToString();
             ammoText.text = ammo + "/" + magAmmo.ToString();
-
-
             Fire();
         }
-        if(Input.GetKeyDown(KeyCode.R) && magAmmo>0)
+        if (Input.GetKeyDown(KeyCode.R) && magAmmo > 0)
         {
             Reload();
-           
-            
         }
-        if (magAmmo == 0 && ammo==0)
+        if (magAmmo == 0 && ammo == 0)
         {
             ammoText.color = Color.red;
         }
-
-        if(recoiling)
+        if (recoiling)
         {
             Recoil();
         }
-        if(recovering)
+        if (recovering)
         {
             Recovering();
         }
 
-
+        // Sürekli raycast çiz
+        DrawContinuousRaycast();
     }
+
     void Reload()
     {
         weaponAnim.Play(reload.name);
-        if(magAmmo>0)
+        if (magAmmo > 0)
         {
-            magAmmo -= fireCount-ammo;
+            magAmmo -= fireCount - ammo;
             ammo = fireCount;
-           
-            
         }
-       
-
-        //magText.text=mag.ToString();
-        ammoText.text=ammo+"/"+magAmmo.ToString();
+        ammoText.text = ammo + "/" + magAmmo.ToString();
     }
+
     void Fire()
     {
-
         recoiling = true;
-        recovering = false; 
+        recovering = false;
 
         Ray ray = new Ray(camera.transform.position, camera.transform.forward);
-
         RaycastHit hit;
 
         if (Physics.Raycast(ray.origin, ray.direction, out hit, 100f))
@@ -139,31 +113,31 @@ public class Weapon : MonoBehaviour
         }
     }
 
+    void DrawContinuousRaycast()
+    {
+        Ray ray = new Ray(camera.transform.position, camera.transform.forward);
+        Debug.DrawRay(ray.origin, ray.direction * 100f, Color.red);
+    }
+
     void Recoil()
     {
-        Vector3 finalPosition=new Vector3(originalPosition.x,originalPosition.y+ recoilUp,originalPosition.z-recoilBack);
-
-        transform.localPosition=Vector3.SmoothDamp(transform.localPosition, finalPosition,ref recoilVelocity,recoilLength); 
-
-        if(transform.localPosition==finalPosition)
+        Vector3 finalPosition = new Vector3(originalPosition.x, originalPosition.y + recoilUp, originalPosition.z - recoilBack);
+        transform.localPosition = Vector3.SmoothDamp(transform.localPosition, finalPosition, ref recoilVelocity, recoilLength);
+        if (transform.localPosition == finalPosition)
         {
             recoiling = false;
-            recovering = true; 
+            recovering = true;
         }
     }
 
     void Recovering()
     {
         Vector3 finalPosition = originalPosition;
-
         transform.localPosition = Vector3.SmoothDamp(transform.localPosition, finalPosition, ref recoilVelocity, recoverLenth);
-
         if (transform.localPosition == finalPosition)
         {
             recoiling = false;
             recovering = false;
         }
-
     }
-
 }
